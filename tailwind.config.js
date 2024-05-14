09b4bd51d84f8dc4c1712cff1b26bf2534af44fd
rare1k <hi@rare1k.dev>
2024-05-14 19:29:20 -0400

    feat: try and use fumadocs dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ import { createPreset } from 'fumadocs-ui/tailwind-plugin';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
@@ -9,7 +10,5 @@ export default {
     './mdx-components.{ts,tsx}',
     './node_modules/fumadocs-ui/dist/**/*.js',
   ],
-  presets: [createPreset({
-    preset: 'ocean',
-  })],
+  presets: [createPreset()],
 };
