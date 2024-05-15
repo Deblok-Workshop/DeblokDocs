1561575981f2bc9ab3f8d7ac58f0ae77bdfecd69
rare1k <hi@rare1k.dev>
2024-05-14 20:42:50 -0400

    feat: im retarded

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import './global.css';
-// import 'fumadocs-ui/style.css';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
 import { Metadata } from 'next';
+
 const inter = Inter({
   subsets: ['latin'],
 });
